102e88c76c4f3b0bd671407c373bd2115f8230b7
Denis Popkov <d.popkov@ltst.ru>
2021-10-23 19:08:03 +0600

    created modules

---
+++ app-domain/src/main/java/ru/popkov/app_domain/MyClass.kt
@@ -0,0 +1,4 @@
+package ru.popkov.app_domain
+
+class MyClass {
+}
---
+++ app-ui/src/main/java/ru/popkov/ui/screens/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.popkov.ui.screens.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.popkov.ui.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
